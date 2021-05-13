c8e714e8f5544f0f8d83817995b142b15988e0b7
Marcin Karwat <markar24@gmail.com>
2021-05-13 17:29:14 +0200

    Cleanup unused files

--- src/main/java/com/students/recipesapi/projection/UserEntityForRegister.java
+++
@@ -1,8 +0,0 @@
-package com.students.recipesapi.projection;
-
-public interface UserEntityForRegister {
-    Long getId();
-    String getUsername();
-    String getFirstName();
-    String getLastName();
-}
