b47f1370299349204bcf52a7c4aef5e2591e7ee9
Marcin Karwat <markar24@gmail.com>
2021-05-27 11:46:58 +0200

    Fix user update model deserialization error

--- src/main/java/com/students/recipesapi/model/UserUpdateModel.java
+++ src/main/java/com/students/recipesapi/model/UserUpdateModel.java
@@ -2,11 +2,13 @@ package com.students.recipesapi.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserUpdateModel {
     private String firstName;
     private String lastName;
